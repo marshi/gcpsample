b24b50d140b805a302d8a8c29268e1f828344647
mukai_masaki <mukai_masaki@cyberagent.co.jp>
2019-09-23 13:39:18 +0900

    initial

---
+++ src/main/kotlin/dev/marshi/gcpsample/GcpsampleApplication.kt
@@ -0,0 +1,20 @@
+package dev.marshi.gcpsample
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+
+@SpringBootApplication
+class GcpsampleApplication
+
+fun main(args: Array<String>) {
+  runApplication<GcpsampleApplication>(*args)
+}
+
+@Component
+class Runner : CommandLineRunner {
+  override fun run(vararg args: String?) {
+    println("hello GCP!!")
+  }
+}
---
+++ src/test/kotlin/dev/marshi/gcpsample/GcpsampleApplicationTests.kt
@@ -0,0 +1,16 @@
+package dev.marshi.gcpsample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class GcpsampleApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
