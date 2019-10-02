ee584e31e07c1ac8dcbe91a672f069d76562730b
mukai_masaki <mukai_masaki@cyberagent.co.jp>
2019-10-02 14:33:33 +0900

    body

--- .github/workflows/pr/entrypoint.sh
+++ .github/workflows/pr/entrypoint.sh
@@ -28,8 +28,9 @@ URI=https://api.github.com
 PULLS_URI="${URI}/repos/$REPO_FULLNAME/pulls"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 TITLE="release to ${DEFAUL_BRANCH}"
+BODY="this pull request is created by github actions."
 
-new_pr_resp=$(curl --data "{\"title\":\"$TITLE\", \"head\": \"$GITHUB_REF\", \"draft\": false, \"base\": \"$DEFAULT_BRANCH\"}" -X POST -s -H "${AUTH_HEADER}" ${PULLS_URI})
+new_pr_resp=$(curl --data "{\"title\":\"$TITLE\", \"head\": \"$GITHUB_REF\", \"body\": \"$BODY\", \"draft\": false, \"base\": \"$DEFAULT_BRANCH\"}" -X POST -s -H "${AUTH_HEADER}" ${PULLS_URI})
 
 echo "$new_pr_resp"
 echo "created pull request"
