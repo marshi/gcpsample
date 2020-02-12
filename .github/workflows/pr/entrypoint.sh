6fc48c74128b1faae2ea3fa5290887fe0e3daefb
mukai_masaki <mashi3648@gmail.com>
2020-02-12 14:48:02 +0900

    Update entrypoint.sh

--- .github/workflows/pr/entrypoint.sh
+++ .github/workflows/pr/entrypoint.sh
@@ -28,9 +28,7 @@ URI=https://api.github.com
 PULLS_URI="${URI}/repos/$REPO_FULLNAME/pulls"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 TITLE="release to ${DEFAULT_BRANCH}"
-BODY=`cat <<EOF
-this pull request is auto created by github actions.
-EOF`
+BODY="this pull request is auto created by github actions."
 
 new_pr_resp=$(curl --data "{\"title\":\"$TITLE\", \"head\": \"$GITHUB_REF\", \"body\": \"$BODY\", \"draft\": false, \"base\": \"$DEFAULT_BRANCH\"}" -X POST -s -H "${AUTH_HEADER}" ${PULLS_URI})
 
